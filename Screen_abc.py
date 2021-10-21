b4b60d3aa61091e1958c64f292f3a6e467ff63be
FratikaK <kamunyan17@gmail.com>
2021-10-21 14:07:31 +0900

    [add]コメント文、エフェクト追加

--- Screen_abc.py
+++ Screen_abc.py
@@ -54,11 +54,15 @@ class Screen_abc(metaclass=abc.ABCMeta):
         pass
 
     def update(self, tick=10):
-        self.draw_effect()
+        self.__draw_effect()
         pygame.display.update()
         clock.tick(tick)
 
-    def draw_effect(self):
+    def __draw_effect(self):
+        """
+        effect_groupにあるエフェクトクラスのエフェクトを表示する
+        privateメソッド
+        """
         effect_group.draw(screen)
         effect_group.update()
 
@@ -101,8 +105,19 @@ class Screen_abc(metaclass=abc.ABCMeta):
         pygame.draw.rect(screen, color, (rightX, posi[1]) + (bold, height))
         pygame.draw.rect(screen, color, (posi[0], leftY) + (width, bold))
 
-    # text:[文字],posi:(左上の座標),bold：boxの線の太さ、pudding:boxの線と一行目の余白
     def setTextBox_S(self, text, textColor, boxColor, posi, width, height, bold=1, size=25, puddingX=10, puddingY=10):
+        """
+        :param text: text:[文字]
+        :param textColor:
+        :param boxColor:
+        :param posi: (左上の座標)
+        :param width:
+        :param height:
+        :param bold: boxの線の太さ
+        :param size:
+        :param puddingX: boxの線と一行目の余白 x
+        :param puddingY: boxの線と一行目の余白 y
+        """
         self.setBox(boxColor, posi, width, height, bold)
         charPosi = (posi[0] + puddingX, posi[1] + puddingY)
         count = 1
@@ -115,55 +130,141 @@ class Screen_abc(metaclass=abc.ABCMeta):
 # -----------------------------------------------------------------------------------
 # 以下エフェクトクラス
 # -----------------------------------------------------------------------------------
-# 画面遷移を行う時に表示するエフェクト
-class ScreenChangeEffect(pygame.sprite.Sprite):
-    def __init__(self, next_screen_id: int):
+class SquareEffect(pygame.sprite.Sprite):
+    BLUE = "blue"
+    RED = "red"
+    color_names = [BLUE, RED]
+
+    def __init__(self, x_location, y_location, color_name: str = "blue"):
+        """
+        マスに止まった時に表示するエフェクト
+
+        :param x_location: 整数のx座標
+        :param y_location: 整数のy座標
+        :param color_name: 色の名前　SquareEffect.REDやSquareEffect.BLUEのように指定してください
+        """
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("img/screen_change.png")
+        self.index = 0
+        self.effect_images = list()
+        if SquareEffect.color_names.__contains__(color_name):
+            self.color_name = color_name
+        else:
+            self.color_name = "blue"
+        for i in range(1, 14):
+            self.effect_images.append(
+                pygame.image.load("img/effect/square/square_effect_" + self.color_name + "_" + str(i) + ".png"))
+        self.image = self.effect_images[self.index]
         self.rect = self.image.get_rect()
-        self.rect.x = 1280
-        self.rect.y = 0
-        self.next_screen_id = next_screen_id
-        self.wait_time = 10
+        self.rect.center = (x_location, y_location)
 
     def update(self):
-        if self.rect.x > 1100:
-            self.rect.x -= 50
-        elif 80 < self.rect.x <= 1100:
-            self.rect.x -= 200
-            if self.rect.x <= 0:
-                self.rect.x = 0
-        elif self.rect.x > 0:
-            self.rect.x -= 50
-            if self.rect.x <= 0:
-                self.rect.x = 0
-
-        elif self.rect.x <= 0:
-            global ScreenNum
-            if ScreenNum != self.next_screen_id:
-                ScreenNum = self.next_screen_id
-            if self.wait_time > 0:
-                if self.rect.x <= 0:
-                    self.rect.x = 0
-                self.wait_time -= 1
-            else:
-                if self.rect.x > -50:
-                    self.rect.x -= 10
-                elif -1800 < self.rect.x <= -50:
-                    self.rect.x -= 300
-                else:
-                    self.rect.x -= 10
-
-            if self.rect.x < -1300:
-                self.remove(effect_group)
+        if self.index >= len(self.effect_images):
+            self.kill()
+            return
+        self.image = self.effect_images[self.index]
+        self.index += 1
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
-# コインが増減する時に出現させるエフェクト
+class ScreenChangeEffect(pygame.sprite.Sprite):
+    SLIDE = "slide"
+    MOSAIC = "mosaic"
+
+    def __init__(self, next_screen_id: int, screen_format: str = SLIDE):
+        """
+        画面遷移を行う時に表示するエフェクト
+
+        ScreenNumを直接編集せずに画面切り替えが可能
+
+        :param next_screen_id: 次に表示したい画面のid
+        :param screen_format: 画面切り替え方式 ScreenChangeEffect.SLIDEや、ScreenChangeEffect.MOSAICのように指定してください
+        """
+        pygame.sprite.Sprite.__init__(self)
+        self.screen_format = screen_format
+        self.index = 0
+        self.next_screen_id = next_screen_id
+        self.wait_time = 10
+        if screen_format == ScreenChangeEffect.MOSAIC:
+            self.images = list()
+            for i in range(1, 12):
+                self.images.append(pygame.image.load("img/screen_change/mosaic/screen_change_mosaic" + str(i) + ".png"))
+            self.limit = len(self.images) - 1
+            self.is_changed = False
+            self.image = self.images[self.index]
+            self.rect = self.image.get_rect()
+        else:
+            self.image = pygame.image.load("img/screen_change/screen_change.png")
+            self.rect = self.image.get_rect()
+            self.rect.x = 1280
+            self.rect.y = 0
+
+    def update(self):
+        global ScreenNum
+        # MOSAIC形式処理
+        if self.screen_format == ScreenChangeEffect.MOSAIC:
+            if not self.is_changed:
+                if self.index > self.limit:
+                    self.is_changed = True
+                    self.index = self.limit
+                    ScreenNum = self.next_screen_id
+                    return
+                self.image = self.images[self.index]
+                self.index += 1
+            elif self.is_changed:
+                if self.wait_time < 0:
+                    if self.index < 1:
+                        self.kill()
+                        return
+                    self.image = self.images[self.index]
+                    self.index -= 1
+                else:
+                    self.wait_time -= 1
+
+        else:  # SLIDE処理
+            if self.rect.x > 1100:
+                self.rect.x -= 50
+            elif 80 < self.rect.x <= 1100:
+                self.rect.x -= 200
+                if self.rect.x <= 0:
+                    self.rect.x = 0
+            elif self.rect.x > 0:
+                self.rect.x -= 50
+                if self.rect.x <= 0:
+                    self.rect.x = 0
+
+            elif self.rect.x <= 0:
+                if ScreenNum != self.next_screen_id:
+                    ScreenNum = self.next_screen_id
+                if self.wait_time > 0:
+                    if self.rect.x <= 0:
+                        self.rect.x = 0
+                    self.wait_time -= 1
+                else:
+                    if self.rect.x > -50:
+                        self.rect.x -= 10
+                    elif -1800 < self.rect.x <= -50:
+                        self.rect.x -= 300
+                    else:
+                        self.rect.x -= 10
+
+                if self.rect.x < -1300:
+                    self.remove(effect_group)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
 class CoinNumFontEffect(pygame.sprite.Sprite):
     def __init__(self, num: int, x_location, y_location):
+        """
+        コインが増減する時に出現させるエフェクト
+
+        :param num: 表示する数字
+        :param x_location: 整数のx座標
+        :param y_location: 整数のy座標
+        """
         pygame.sprite.Sprite.__init__(self)
         if num > 0:
             self.color = (0, 255, 255)
@@ -191,9 +292,14 @@ class CoinNumFontEffect(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
 
-# 文字を右から左へ移動させるアニメーション
 class ScreenLineFontEffect(pygame.sprite.Sprite):
     def __init__(self, message: str, color=(255, 255, 255)):
+        """
+        文字を右から左へ移動させるアニメーション
+
+        :param message: 表示したい文字
+        :param color: 色　Screen_abcのstaticカラー指定可能
+        """
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.SysFont("yugothicyugothicuisemiboldyugothicuibold", 70)
         self.image = self.font.render(message, True, color)
@@ -215,6 +321,9 @@ class ScreenLineFontEffect(pygame.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
 
+# ------------------------------------------------
+# 以下ボツエフェクト
+# ------------------------------------------------
 # ウマが歩くだけのアニメーション
 class UmaEffect(pygame.sprite.Sprite):
     def __init__(self):
@@ -250,9 +359,9 @@ class IceEffect(pygame.sprite.Sprite):
         self.effect_images = list()
         for i in range(1, 22):
             if i < 10:
-                self.effect_images.append(pygame.image.load("img/effect/sample_effect_00" + str(i) + ".png"))
+                self.effect_images.append(pygame.image.load("img/effect/ice/sample_effect_00" + str(i) + ".png"))
             else:
-                self.effect_images.append(pygame.image.load("img/effect/sample_effect_0" + str(i) + ".png"))
+                self.effect_images.append(pygame.image.load("img/effect/ice/sample_effect_0" + str(i) + ".png"))
         self.image = self.effect_images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = (x_location, y_location)
@@ -275,7 +384,7 @@ class CircleEffect(pygame.sprite.Sprite):
         self.index = 0
         self.effect_images = list()
         for i in range(1, 16):
-            self.effect_images.append(pygame.image.load("img/circle/circle_effect" + str(i) + ".png"))
+            self.effect_images.append(pygame.image.load("img/effect/circle/circle_effect" + str(i) + ".png"))
         self.image = self.effect_images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = (x_location, y_location)
